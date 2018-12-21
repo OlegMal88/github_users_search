b52b9e3912e60ca18adc4590d559eae99f1780e0
Danylo <Danylo_Bilokha@epam.com>
2018-12-21 19:43:39 +0200

    search emit searchValue.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -3,12 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.template.html',
-  styleUrls: ['./app.styles.scss']
 })
 class AppComponent {
+
   onSearchChange(searchQuery: string): void {
     console.log(searchQuery);
   }
+
 }
 
 export {AppComponent};
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -3,16 +3,15 @@ import {Component, EventEmitter, Output} from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.template.html',
-  styleUrls: ['./search.styles.css']
 })
 class SearchComponent {
-
   @Output()
   public searchChange: EventEmitter<string> = new EventEmitter();
 
-  public searchQuery: string = '';
+  public searchQuery: string;
 
   public onSearch(): void {
+    console.log('here', this.searchQuery)
     this.searchChange.emit(this.searchQuery);
   }
 }
