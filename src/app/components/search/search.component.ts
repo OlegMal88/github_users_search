67d0f49fe92c05536a79d1a337abe7412966d516
Danylo <Danylo_Bilokha@epam.com>
2018-12-21 19:18:18 +0200

    added blank search

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, OnDestroy, Output} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Subscription} from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.template.html',
+  styleUrls: ['./search.styles.css']
+})
+export class SearchComponent implements OnDestroy {
+
+  @Output()
+  public searchChange: EventEmitter<string> = new EventEmitter();
+
+  public searchQuerySink: Subject<string> = new Subject();
+  private searchQuerySubscription: Subscription = this.searchQuerySink
+    .subscribe((searchQuery: string) => this.searchChange.emit(searchQuery));
+
+  ngOnDestroy(): void {
+    this.searchQuerySubscription.unsubscribe();
+  }
+}
