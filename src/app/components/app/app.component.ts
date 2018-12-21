827514875503b0010e428ea579b687451fa1f3bc
oleg_malyshev <oleg_malyshev@epam.com>
2018-12-21 19:20:45 +0200

    add styles

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.template.html',
-  styleUrls: ['./app.styles.css']
+  styleUrls: ['./app.styles.scss']
 })
 export class AppComponent {
   title = 'app';
